37df433bbbd607b9cbc40c64d28bc5dff9ae68fb
Leonardo <leonardosiqueira387@gmail.com>
2022-10-17 11:42:55 -0300

    e5

---
+++ e5_fund_prog.java
@@ -0,0 +1,133 @@
+public class e5_fund_prog {
+    public static void main(String args[]){
+
+        Square casa = new Square();
+        Triangle teto = new Triangle();
+        Square chao = new Square();
+        Square chao1 = new Square();
+        Square chao2 = new Square();
+        Square chao3 = new Square();
+        Square chao4 = new Square();
+        Square chao5 = new Square();
+        Square chao6 = new Square();
+        Square chao7 = new Square();
+        Square chao8 = new Square();
+        Square chao9 = new Square();
+        Square chao10 = new Square();
+        Square chao11 = new Square();
+        Square chao12 = new Square();
+        Square chao13 = new Square();
+        Square chao14 = new Square();
+        Square chao15 = new Square();
+        Square sol = new Square();
+        Square porta = new Square();
+        Square porta1 = new Square();
+
+        
+        casa.makeVisible();
+        casa.changeSize(130);
+        casa.changeColor("blue");
+        casa.moveVertical(100);
+
+        teto.makeVisible();
+        teto.changeSize(134, 134);
+        teto.changeColor("black");
+        teto.moveHorizontal(74);
+        teto.moveVertical(3);
+
+        sol.makeVisible();
+        sol.changeColor("yellow");
+        sol.moveHorizontal(210);
+        sol.moveVertical(-50);
+
+        porta.makeVisible();
+        porta.changeColor("yellow");
+        porta.moveHorizontal(50);
+        porta.moveVertical(190);
+
+        porta1.makeVisible();
+        porta1.changeColor("yellow");
+        porta1.moveHorizontal(50);
+        porta1.moveVertical(170);
+        
+        chao.makeVisible();
+        chao.changeColor("green");
+        chao.moveVertical(220);
+
+        chao1.makeVisible();
+        chao1.changeColor("green");
+        chao1.moveVertical(220);
+        chao1.moveHorizontal(30);
+
+        chao2.makeVisible();
+        chao2.changeColor("green");
+        chao2.moveVertical(220);
+        chao2.moveHorizontal(50);
+
+        chao3.makeVisible();
+        chao3.changeColor("green");
+        chao3.moveVertical(220);
+        chao3.moveHorizontal(70);
+
+        chao4.makeVisible();
+        chao4.changeColor("green");
+        chao4.moveVertical(220);
+        chao4.moveHorizontal(90);
+
+        chao5.makeVisible();
+        chao5.changeColor("green");
+        chao5.moveVertical(220);
+        chao5.moveHorizontal(110);
+        
+        chao6.makeVisible();
+        chao6.changeColor("green");
+        chao6.moveVertical(220);
+        chao6.moveHorizontal(130);
+
+        chao7.makeVisible();
+        chao7.changeColor("green");
+        chao7.moveVertical(220);
+        chao7.moveHorizontal(150);
+
+        chao8.makeVisible();
+        chao8.changeColor("green");
+        chao8.moveVertical(220);
+        chao8.moveHorizontal(170);
+
+        chao9.makeVisible();
+        chao9.changeColor("green");
+        chao9.moveVertical(220);
+        chao9.moveHorizontal(190);
+
+        chao10.makeVisible();
+        chao10.changeColor("green");
+        chao10.moveVertical(220);
+        chao10.moveHorizontal(210);
+
+        chao11.makeVisible();
+        chao11.changeColor("green");
+        chao11.moveVertical(220);
+        chao11.moveHorizontal(10);
+
+        chao12.makeVisible();
+        chao12.changeColor("green");
+        chao12.moveVertical(220);
+        chao12.moveHorizontal(-10);
+
+        chao13.makeVisible();
+        chao13.changeColor("green");
+        chao13.moveVertical(220);
+        chao13.moveHorizontal(-30);
+
+        chao14.makeVisible();
+        chao14.changeColor("green");
+        chao14.moveVertical(220);
+        chao14.moveHorizontal(-50);
+
+        chao15.makeVisible();
+        chao15.changeColor("green");
+        chao15.moveVertical(220);
+        chao15.moveHorizontal(-70);
+
+    }
+}
